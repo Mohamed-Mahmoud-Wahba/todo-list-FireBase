b6c63c89a22a6c5469a2e7cebf02b84a14332f6a
mohamed wahba <mohamed.wahba@baianat.com>
2022-12-31 10:21:03 +0200

    feat : add env variables

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCaByhJA7oNzVFjMUTD_waQOFd2pW6S2C0",
-  authDomain: "todo-list-d9378.firebaseapp.com",
-  projectId: "todo-list-d9378",
-  storageBucket: "todo-list-d9378.appspot.com",
-  messagingSenderId: "281705380017",
-  appId: "1:281705380017:web:50efc58a85b442759b0a90"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 const app = initializeApp(firebaseConfig);
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,13 @@
 export default {
+
+  env:{
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'app',
